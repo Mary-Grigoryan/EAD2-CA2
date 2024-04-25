c0ff658dbe8adff6546001a93658681c16358709
Mary-Grigoryan <x00178070@mytudublin.ie>
2024-04-25 13:52:35 +0100

    fixing library entry backend

--- BookTrackerApi/Migrations/20240425125210_Test.Designer.cs
+++ BookTrackerApi/Migrations/20240425125210_Test.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookTrackerApi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240425122339_Test")]
+    [Migration("20240425125210_Test")]
     partial class Test
     {
         /// <inheritdoc />
--- BookTrackerApi/Models/LibraryEntry.cs
+++ BookTrackerApi/Models/LibraryEntry.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization; // Make sure you have this using directive
 
 namespace BookTrackerApi.Models
 {
@@ -7,13 +8,15 @@ namespace BookTrackerApi.Models
     {
         [Key]
         public int Id { get; set; }
-        
+
         [ForeignKey("Book")]
         public int BookId { get; set; }
+
+        [JsonIgnore] // prevent serialization/deserialization
         public Book Book { get; set; }
-        
+
         public string UserId { get; set; } // Assume we have user identification in place
-        
+
         public string ReadingStatus { get; set; } // "to read", "reading now", "have read"
     }
 }
